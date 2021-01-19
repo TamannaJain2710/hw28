96e430e6834b587be46460282436915fbac2b8f7
Tamanna <thecreativefeather725@gmail.com>
2021-01-19 17:23:51 +0530

    .

--- constraint.js
+++ constraint.js
@@ -1,26 +1,30 @@
 class SlingShot{
-    constructor(bodyA, pointB){
-        var options = {
-            bodyA: bodyA,
-            pointB: pointB,
-            stiffness: 0.04,
-            length: 10
-        }
-
-        this.sling = Constraint.create(options);
-        World.add(world, this.sling);
-    }
-
-    display(){
+    constructor(bodyA, pointB) {
+      var options = {
+        bodyA: bodyA,
+        pointB: pointB,
+        stiffness: 0.004,
+        lenght: 50
+      } 
+      this.pointB = pointB;
+      this.sling = Constraint.create(options);
+      World.add(world,this.sling);
+      }
+      display(){
         if(this.sling.bodyA){
-        var pointA = this.sling.bodyA.position;
-        var pointB = this.sling.pointB;
         strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        line(pointA.x, pointA.y, pointB.x, pointB.y); 
         }
-    }
-   fly() {
-        this.sling.bodyA = null
-        
-    }
+      }
+
+      fly(){
+        this.sling.bodyA = null;
+      }
+      attach(body){
+        this.sling.bodyA = body;
+
+      }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,32 +1,40 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-var treeObj, stoneObj,groundObject, launcherObject;
-var mango1,COnstraint;
-var world,boy;
+const Render = Matter.Render;
+
+var engine, world;
+var treeObj, stone,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6;
+var elastic;
+var boy;
 
 function preload(){
-	boy=loadImage("boy.png");
+	boy = loadImage("boy.png");
 	
   }
 
 function setup() {
 	createCanvas(1300, 600);
-	engine = Engine.create();
-	world = engine.world;
+	  engine = Engine.create();
+    world = engine.world
 
-	mango1=new mango(1100,100,30);
+  mango1=new mango(1200,120,30);
+  mango2=new mango(1100,200,30);
+  mango3=new mango(1050,80,30);
+  mango4=new mango(950,150,30);
+  mango5=new mango(970,270,30);
+  mango6=new mango(1200,270,30);
 
 	treeObj=new tree(1050,580);
 	groundObject=new ground(width/2,600,width,20);
-	stoneObj = new Stone(300,300,10,5);
-	COnstraint = new SlingShot(stoneObj.body,{x:300,y:300});
 	
-	//Engine.run(engine);
+	  stone = new Stone(300,300,10,5);
+   // elastic =  new SlingShot(stone.body, {x:280 , y:575});
+	
+	Engine.run(engine);
 
 }
 
@@ -37,9 +45,16 @@ function draw() {
   //Add code for displaying text here!
   image(boy ,200,340,200,300);
   
-  stoneObj.display();
+  stone.display();
   treeObj.display();
+  treeObj.depth = mango5.depth
+  treeObj.depth = treeObj.depth - 1
   mango1.display();
-  COnstraint.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
   groundObject.display();
+  //elastic.display();  
 }
